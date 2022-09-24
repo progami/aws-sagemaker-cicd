7a1973ad05831c20db9aab9a473f137d9b7ca41c
jarrar <jarraramjad@gmail.com>
2022-09-24 16:23:50 +0500

    updated lambda get code

--- src/lambda_get.py
+++ src/lambda_get.py
@@ -3,18 +3,20 @@ import boto3
 
 s3 = boto3.client('s3')
 
+BUCKET_NAME = "123124-zepto-s3-bucket"
+FILE_NAME   = "boston-housing/reports.csv"
+
 def lambda_handler(event, context):
 
-    bucket_name = event ["pathParameters"]["bucket"]
-    file_name = event ["queryStringParameters"]["file"]
-    fileObj = s3.get_object(Bucket=bucket_name, Key=file_name)
+
+    fileObj = s3.get_object(Bucket=BUCKET_NAME, Key=FILE_NAME)
     file_content = fileObj["Body"].read()
     # print
     return {
         "statusCode": 200,
         "headers": {
             "Content-type": "text/html",
-            "Content-Disposition": "attachment; filename={}".format(file_name)
+            "Content-Disposition": "attachment; filename={}".format(FILE_NAME)
         },
         "body": json.dumps(file_content)
     }
