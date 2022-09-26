a6fa247bdb9615ed0863cecfb47d35fc2dcb42dc
jarrar <jarraramjad@gmail.com>
2022-09-26 12:09:18 +0500

    test get function

--- infra/src/lambda_get.py
+++ infra/src/lambda_get.py
@@ -9,19 +9,19 @@ runtime= boto3.client('runtime.sagemaker')
 
 def lambda_handler(event, context):
     
-    # print("Received event: " + json.dumps(event, indent=2))
+    # # print("Received event: " + json.dumps(event, indent=2))
     
-    data = json.loads(json.dumps(event))
-    payload = data['data']
+    # data = json.loads(json.dumps(event))
+    # payload = data['data']
     
-    print(payload)
+    # print(payload)
     
-    # response = runtime.invoke_endpoint(EndpointName=ENDPOINT_NAME,
-    #                                     Body=json.dumps(payload))
-    # print(response)
-    # result = json.loads(response['Body'].read().decode())
-    # print(result)
+    # # response = runtime.invoke_endpoint(EndpointName=ENDPOINT_NAME,
+    # #                                     Body=json.dumps(payload))
+    # # print(response)
+    # # result = json.loads(response['Body'].read().decode())
+    # # print(result)
     
-    print(payload[0][-1])
+    # print(payload[0][-1])
 
     return {'statusCode': 200, 'body': json.dumps(ENDPOINT_NAME)}
