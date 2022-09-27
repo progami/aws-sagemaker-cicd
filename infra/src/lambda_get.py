92e2b39240796d4c45fa64ff49dce419977d0bdf
jarrar <jarraramjad@gmail.com>
2022-09-27 10:14:36 +0500

    api gateway working integration

--- infra/src/lambda_get.py
+++ infra/src/lambda_get.py
@@ -4,24 +4,21 @@ import boto3
 import json
 
 # grab environment variables
-ENDPOINT_NAME = "https://runtime.sagemaker.eu-central-1.amazonaws.com/endpoints/boston-housing-model-2022-09-26-02-51-02-308/invocations"
+ENDPOINT_NAME = "https://runtime.sagemaker.eu-central-1.amazonaws.com/endpoints/boston-housing-model-2022-09-26-12-37-50-378/invocations"
 runtime= boto3.client('runtime.sagemaker')
 
 def lambda_handler(event, context):
+    print("Received event: " + json.dumps(event, indent=2))
     
-    # # print("Received event: " + json.dumps(event, indent=2))
+    data = json.loads(json.dumps(event))
+    payload = data['data']
+    print(payload)
     
-    # data = json.loads(json.dumps(event))
-    # payload = data['data']
+    response = runtime.invoke_endpoint(EndpointName=ENDPOINT_NAME,
+                                       Body=json.dumps(payload))
+    print(response)
+    result = json.loads(response['Body'].read().decode())
+    print(result)
     
-    # print(payload)
-    
-    # # response = runtime.invoke_endpoint(EndpointName=ENDPOINT_NAME,
-    # #                                     Body=json.dumps(payload))
-    # # print(response)
-    # # result = json.loads(response['Body'].read().decode())
-    # # print(result)
-    
-    # print(payload[0][-1])
-
-    return {'statusCode': 200, 'body': json.dumps(ENDPOINT_NAME)}
+    return result[0]
+    # return {'statusCode': 200, 'body': json.dumps(ENDPOINT_NAME)}
