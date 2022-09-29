0cab5942fd8f184ccd3d92285207881f8f2b5a4c
jarrar <jarraramjad@gmail.com>
2022-09-29 12:44:29 +0500

    modifying for dynamo integration, new file structure is acceptable

--- infra/instream.tf
+++ infra/instream.tf
@@ -1,3 +1,66 @@
+###############################LAMBDA#################################
+
+resource "aws_iam_role" "lambda_in_dynamodb_role_tf" {
+  name = "lambda_in_dynamodb_tf"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "lambda.amazonaws.com"
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_role_policy" "lambda_in_dynamodb_policy" {
+  name = aws_iam_role.lambda_in_dynamodb_role_tf.name
+  role = aws_iam_role.lambda_in_dynamodb_role_tf.id
+
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": [
+        "s3:*",
+        "dynamodb:*",
+      ],
+      "Effect": "Allow",
+      "Resource": "*"
+    }
+  ]
+}
+EOF
+}
+
+
+# Creating Lambda resource for fetching data from dynmodb instream
+resource "aws_lambda_function" "lambda_in_dynamodb" {
+  function_name               = "lambda_in_dynamodb"
+  role                        = aws_iam_role.lambda_in_dynamodb_role_tf.arn
+  handler                     = "lambda_in_dynamodb.lambda_handler"
+  runtime                     = "python3.9"
+  timeout                     = 5
+  filename                    = "src/lambda_in_dynamodb.zip"
+  source_code_hash            = filebase64sha256("src/lambda_in_dynamodb.zip")
+  environment {
+    variables = {
+      env                     = "dev"
+      SENDER_EMAIL            = "jarraramjad@gmail.com"
+      RECEIVER_EMAIL          = "jarraramjad@gmail.com"
+      region                  = var.aws_region
+      SNS_ARN                 = aws_sns_topic.topic.arn
+    }
+  }
+}
+
 
 ###############################S3 Bucket###############################
 
